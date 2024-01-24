208800c248aba27a2a8cdb93c2140ce037d9cdae
Carolaine Machado <carolainemachado12345@gmail.com>
2024-01-23 22:24:10 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,20 @@
 package application;
 
-import java.util.Scanner;
+
+import java.util.Date;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
 		
 		Department obj = new Department (1,"Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller (21,"Bob","bob@gmail.com",new Date(), 3000.0, obj);
+		
+		//System.out.println(obj);
+		System.out.println(seller);
 	}
 }
